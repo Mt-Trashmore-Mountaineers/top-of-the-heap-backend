f339468435ebc7951b021235ce1331455a7315ce
jordanLindo <jordanlindo@live.com>
2022-08-17 13:15:50 -0500

    Addded backend post

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.use(express.json());
 const cors = require('cors');
 app.use(cors());
 
+const Quiz = require('./models/quiz.js')
+
+
 //MONGOOSE CONNECTION
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URL);
@@ -19,5 +22,25 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
   console.log('Mongoose is connected');
 });
+
+
+//ROUTES 
+app.get('/',(req, res) => {
+  res.status(200).send("Welcome, Mongoose is connected.");
+})
+
+
+app.post('/new',postQuiz);
+
+
+async function postQuiz(req,res,next){
+  console.log('!! quiz req.body', req.body);
+  try {
+    let createdQuiz = await Quiz.create(req.body);
+      res.status(200).send(createdQuiz);
+  } catch (error) {
+    next(error)
+  }
+}
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
