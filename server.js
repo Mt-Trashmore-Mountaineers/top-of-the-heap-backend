d468575d292eb9d28edbb9ec41783ea4fb0ec880
jordanLindo <jordanlindo@live.com>
2022-08-18 10:41:11 -0500

    Added db query routes byEmail and byId

--- server.js
+++ server.js
@@ -32,12 +32,48 @@ app.get('/',(req, res) => {
 
 app.post('/new',postQuiz);
 
+app.get('/quiz/email', getQuizSet);
+
+app.get('/quiz', getQuizzes);
+
+app.get('/quiz/id', getQuizById);
+
 
 async function postQuiz(req,res,next){
   console.log('!! quiz req.body', req.body);
   try {
     let createdQuiz = await Quiz.create(req.body);
       res.status(200).send(createdQuiz);
+  } catch (error) {
+    next(error)
+  }
+}
+
+async function getQuizById(req,res,next){
+  let id = req.query.id;
+  
+  try {
+    let quiz = await Quiz.findById(id);
+      res.status(200).send(quiz);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function getQuizSet(req,res,next){
+  let reqEmail = req.query.email;
+  try {
+    let results = await Quiz.find({email: reqEmail});
+    res.status(200).send(results);
+  } catch (error) {
+    next(error)
+  }
+}
+
+async function getQuizzes(req,res,next){
+  try {
+    let results = await Quiz.find({});
+    res.status(200).send(results);
   } catch (error) {
     next(error)
   }
